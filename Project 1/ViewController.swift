fe4739ab905bb7a54822c7889c191c20d2c19f14
GWC <gwc@cs81-us-mus-bb.local>
2019-07-16 13:39:09 -0400

    Initial Commit

---
+++ Project 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 1
+//
+//  Created by GWC on 7/16/19.
+//  Copyright © 2019 GWC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
